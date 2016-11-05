807ddf9b0ab4fa2c540224a72e35c9f7fff6b998
Alex .P <persson.ac@gmail.com>
2016-11-04 20:38:45 -0400

    Card problem

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,7 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // TODO: Create IB outlets
+    
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var suitLabel: UILabel!
+    
+    @IBOutlet weak var cardButton: UIButton!
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if let label = sender.currentTitle {
+            var labelArray = Array(label.characters)
+            let suite = String(labelArray[0])
+            var labelArrayWithoutSuite = labelArray.dropFirst()
+            let value = String(labelArrayWithoutSuite)
+            print (labelArray)
+            print(suite)
+            print(value)
+            updateSuite(suite: suite)
+            updateNumber(label: value)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +38,14 @@ class ViewController: UIViewController {
     
     
     // TODO: IB actions and code to update UI
-
+    func updateSuite(suite: String) {
+        suitLabel.text = suite
+    }
+    
+    func updateNumber(label: String) {
+        numberLabel.text = label
+    }
+    
   
 
 }
